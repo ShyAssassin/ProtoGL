708bc25c61d4f5b6034eee15649ebbcf963e12e5
[Assassin] <ShyAssassin@assassin.dev>
2026-04-15 22:19:19 +0200

    Hello Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
+#include <print>
 #include <iostream>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (!glfwInit()) {
+        std::print("Failed to initialize GLFW");
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "ProtoGL", nullptr, nullptr);
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
+        std::print("Failed to initialize GLAD");
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     return 0;
 }
